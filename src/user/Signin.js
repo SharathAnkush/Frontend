94dd3d894cd12d7bd40bdcc93dd07258e369c6a6
sharathankush <sharathankush@gmail.com>
2021-03-26 22:31:14 +0530

    added Signin component talks to backend and nav signout link

--- src/user/Signin.js
+++ src/user/Signin.js
@@ -1,8 +1,76 @@
 import React, { useState } from "react";
 import Base from "../core/Base";
-import { Link } from "react-router-dom";
+import { Link, Redirect } from "react-router-dom";
+import { Authenticate, isAuthenticated, signin } from "../auth/helper";
 
 const Signin = () => {
+
+  const [values, setValues] = useState({
+    email: "sharath@a.com",
+    password: "12345",
+    error: "",
+    loading: false,
+    didRedirect : false
+  })
+
+  const {email, password, error, loading, didRedirect} = values;
+  const {user} = isAuthenticated() ;
+
+  const handelChange = email => event => {
+     setValues({...values, error:false, [email]: event.target.value })
+  }
+
+  const onSubmit = event => {
+    event.preventDefault();
+    setValues({...values, error: false, loading:true})
+    signin({email,password})
+    .then(data => {
+      if(data?.error){
+        setValues({...values, error:data?.error, loading:false})
+      } else {
+        Authenticate(data, () => {
+          setValues({...values, didRedirect:true })
+        })
+      }
+    })
+    .catch(console.log("signin request failed"))
+  }
+  
+  const performRedirect = () => {
+    if(didRedirect){
+      if(user && user.role === 1){
+        return <Redirect to="/admin"/>;   // <p>redirect to Admin</p>;
+      } else {
+        return <Redirect to="/admin/dashboard"/>;//<p>redirect to User Dash board</p>;
+      }
+    }
+    if(Authenticate()){
+      return <Redirect to="/" />
+    }
+  }
+
+  const loadingMessage = () => {
+    return (
+         loading && (
+            <div className='alert alert-info'>
+              <h1>Loading...</h1>
+            </div>)
+    );
+  }
+
+  const errorMessage = () => {
+    return ( 
+      <div className="row">
+        <div className="col-md-6 offset-sm-3 text-left">
+          <div className='alert alert-danger'
+              style={{ display: error ? "" : "none" }} >
+            {error} 
+          </div>
+        </div>
+      </div>
+    );
+  }
+
   const signInForm = () => {
     return (
       <div className="row">
@@ -10,14 +78,14 @@ const Signin = () => {
           <form>
             <div className="form-group">
               <label className="text-light">Email</label>
-              <input className="form-control" type="email" />
+              <input className="form-control" onChange={handelChange("email")} value={email} type="email" />
             </div>
 
             <div className="form-group">
               <label className="text-light">Password</label>
-              <input className="form-control" type="password" />
+              <input className="form-control" onChange={handelChange("password")} value={password} type="password" />
             </div>
-            <button className="btn btn-success btn-block">Submit</button>
+            <button className="btn btn-success btn-block" onClick={onSubmit}>Submit</button>
           </form>
         </div>
       </div>
@@ -26,7 +94,11 @@ const Signin = () => {
 
   return (
     <Base title="Sign In page" description="A page for user to sign in!">
+      {loadingMessage()}
+      {errorMessage()}
       {signInForm()}
+      {performRedirect()}
+      
     </Base>
   );
 };
